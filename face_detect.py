de3608dacb6a3ab26dcf54b4c3f725e6623f22b5
Ashish Shukla <49155569+shuklaashish057@users.noreply.github.com>
2020-05-12 14:44:39 +0530

    Add files via upload

---
+++ face_detect.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+import os 
+import pyttsx3
+import engineio
+
+engineio = pyttsx3.init()
+voices = engineio.getProperty('voices')
+engineio.setProperty('rate', 130)  
+voice_id = "hindi"
+engineio.setProperty('voice',voice_id)
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "Cascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+font = cv2.FONT_HERSHEY_SIMPLEX
+id = 0
+names = ['None','Ashish', 'Apurva', 'Ankit'] 
+cam = cv2.VideoCapture(0)
+cam.set(3, 1250) 
+cam.set(4, 1250) 
+
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+while True:
+    ret, img =cam.read()
+    img = cv2.flip(img, 1)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w]) 
+        if True:
+            id = names[id]
+            cv2.putText(
+                    img, 
+                    str(id), 
+                    (x+5,y-5), 
+                    font, 
+                    1, 
+                    (255,255,255), 
+                    2
+                   )
+            break
+        else:
+            id = "unknown"
+            cv2.putText(
+                        img, 
+                        str(id), 
+                        (x+5,y-5), 
+                        font, 
+                        1, 
+                        (255,255,255), 
+                        2
+                    )
+        
+      
+    
+    cv2.imshow('camera',img) 
+    k = cv2.waitKey(10) & 0xff 
+    if k == 27:
+        break
+    
+engineio.say('Thank you {0}'.format(id))
+engineio.runAndWait()
+print("\n Thank you for the test.")
+cam.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
---
+++ face_training.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os
+# Path for face image database
+path = 'dataset'
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier("Cascades/haarcascade_frontalface_default.xml")
+# function to get the images and label data
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]    
+    faceSamples=[]
+    ids = []
+    for imagePath in imagePaths:
+        PIL_img = Image.open(imagePath).convert('L') 
+        img_numpy = np.array(PIL_img,'uint8')
+        id = int(os.path.split(imagePath)[-1].split(".")[1]) 
+        faces = detector.detectMultiScale(img_numpy)
+        print("faces", faces)
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+            
+    return faceSamples,ids
+
+print ("\n Wait for a second...")
+faces,ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+
+recognizer.write('trainer/trainer.yml') 
+
+print("\n {0} faces trained. Exiting Program".format(len(np.unique(ids))))
