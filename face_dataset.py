d178c32b59a17458ed7b0ee9dc9c605d0accf1d6
Ashish Shukla <49155569+shuklaashish057@users.noreply.github.com>
2020-05-12 14:47:58 +0530

    Update face_dataset.py

--- face_dataset.py
+++ face_dataset.py
@@ -3,7 +3,7 @@ import os
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) 
 cam.set(4, 480) 
-face_detector = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 face_id = input('\n enter user id end press <return> ==>  ')
 print("\n Initializing face capture. Look the camera and wait ...")
