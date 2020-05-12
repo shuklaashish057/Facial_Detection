6a63038bc061af4b6275b9435c12eff5425491eb
Ashish Shukla <49155569+shuklaashish057@users.noreply.github.com>
2020-05-12 14:48:58 +0530

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
-faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
-eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
-smileCascade = cv2.CascadeClassifier('Cascades/haarcascade_smile.xml')
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # set Width
